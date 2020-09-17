aa61649a55b5d89f7a69054ec151eaa99f240b37
Pandamba <71420257+Pandamba@users.noreply.github.com>
2020-09-17 11:50:06 +0800

    Update install_v2_docker.sh

--- install_v2_docker.sh
+++ install_v2_docker.sh
@@ -5,12 +5,7 @@ suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
 mkdir /tmp/$suijishu
 cd /tmp/$suijishu
 wget https://raw.githubusercontent.com/Pandamba/V2Ray/master/docker-compose.yml
-read -p " 请输入容器名字：" dockername
-echo "面板地址如：http://www.baidu.com"
-read -p "请输入你的面板地址后面不要带斜杠'/'：" dockerurl
-read -p "请输入你的面板KEY：" dockerkey
 read -p "请输入你的节点ID：" dockerid
-read -p "请输入你的容器端口：" dockerport
 #安装docker
 curl -sSL https://get.docker.com | bash
 service docker start
@@ -20,11 +15,11 @@ chmod +x /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 clear
 #配置文件
-sed -i "s|HIM|$dockername|" docker-compose.yml
-sed -i "s|sspankey|$dockerkey|" docker-compose.yml
+sed -i "s|panda|$dockername|" docker-compose.yml
+sed -i "s|panda|$dockerkey|" docker-compose.yml
 sed -i "s|68|$dockerid|" docker-compose.yml
-sed -i "s|5109|$dockerport|" docker-compose.yml
-sed -i "s|https://sjiuishuhfeufuebfububncissn.tiyunzong.club|$dockerurl|" docker-compose.yml
+sed -i "s|9898|$dockerport|" docker-compose.yml
+sed -i "s|https://Panda.mba|$dockerurl|" docker-compose.yml
 docker-compose up -d
 docker ps
 echo $suijishu
